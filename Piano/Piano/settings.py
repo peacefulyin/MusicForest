cdcb589ba971bf394acca1f1947787da4547818e
Monkey <731769168@qq.com>
2017-09-23 22:11:47 +0800

    deploy done

--- Piano/Piano/settings.py
+++ Piano/Piano/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+lsb9g%-qhc9iv)l2c6_i*vua2huw&ek!=nysadz&$l7+0dqj('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
